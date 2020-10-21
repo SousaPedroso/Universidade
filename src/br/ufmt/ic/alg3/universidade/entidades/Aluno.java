d0b2477c2240b73201fb9be94ff44a74822fdee8
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2020-10-21 15:46:25 -0400

    Update: Atributo cpf à entidade Aluno

--- src/br/ufmt/ic/alg3/universidade/entidades/Aluno.java
+++ src/br/ufmt/ic/alg3/universidade/entidades/Aluno.java
@@ -7,6 +7,7 @@ package br.ufmt.ic.alg3.universidade.entidades;
 
 public class Aluno {
     private String nome;
+    private String cpf;
     private String email;
     private String rga;
 
@@ -16,6 +17,14 @@ public class Aluno {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public String getEmail() {
